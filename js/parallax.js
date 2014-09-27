3b84a319fc69f13477542bcdb34eee7d64004489
oliviercd <olivier.chan@gmail.com>
2014-09-27 18:04:32 -0400

    Separated jss and css

--- js/parallax.js
+++ js/parallax.js
@@ -1,16 +1,17 @@
-$(document).ready(function()
-{
-    $(window).bind('scroll',function(e)
-    {
-        parallaxScroll();
+(function(){
+
+  var parallax = document.querySelectorAll(".parallax"),
+      speed = 0.5;
+
+  window.onscroll = function(){
+    [].slice.call(parallax).forEach(function(el,i){
+
+      var windowYOffset = window.pageYOffset,
+          elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
+      
+      el.style.backgroundPosition = elBackgrounPos;
+
     });
- 
-    function parallaxScroll()
-	{
-        var scrolledY = $(window).scrollTop();
-        $('.background').css('background-position','center -'+((scrolledY*0.2))+'px');
-        $('.bluestripe').css('top','-'+((scrolledY*0.5))+'px');
-        $('.greenstripe').css('top','-'+((scrolledY*0.8))+'px');
-    }
- 
-});
+  };
+
+})();
